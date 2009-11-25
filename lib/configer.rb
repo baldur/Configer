594114f74c1ca07a66b33fcd60a23c756556f973
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2009-11-24 22:28:26 -0500

    adding inital working model

--- lib/configer.rb
+++ lib/configer.rb
@@ -1,6 +1,26 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rubygems'
+require 'yaml'
+require 'ruby-debug'
+
 module Configer
   VERSION = '0.0.1'
+
+  def self.init(path=File.dirname(__FILE__) + "/../spec/fixtures/")
+    Dir["#{path}/*.yml","#{path}/*.yaml"].each do |path_to_config_file|
+      config = File.basename(path_to_config_file).gsub(/\.y(a)?ml/, "")
+      const_set(config.upcase, YAML.load_file(path_to_config_file))
+    end
+  end
+
+  def self.destroy_constants
+    constants.each do |constant|
+      unless constant == "VERSION"
+        remove_const constant.to_sym
+      end
+    end
+  end
 end
+
--- spec/configer_spec.rb
+++ spec/configer_spec.rb
@@ -1,11 +1,43 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+describe "after initialization" do
+  before do
+    Configer.init
+    @hosts = YAML.load_file(File.dirname(__FILE__) + "/../spec/fixtures/hosts.yaml")
+    @exernal_services = YAML.load_file(File.dirname(__FILE__) + "/../spec/fixtures/external_services.yaml")
+  end
   
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  it "should have HOSTS constance" do
+    lambda { Configer::HOSTS }.should_not raise_error NameError
+  end
+
+  it "should have EXTERNAL_SERVICES constance" do
+    lambda { Configer::EXTERNAL_SERVICES }.should_not raise_error NameError
+  end
+
+  describe "hosts.yaml" do
+    it "should match database" do
+      @hosts['database'].each_pair do |key, value|
+      Configer::HOSTS['database'][key].should == value
+      end
+    end
+
+    it "should match search_node" do
+      @hosts['search_node'].each_pair do |key, value|
+      Configer::HOSTS['search_node'][key].should == value
+      end
+    end
+  end
+
+  describe "external_services.yaml" do
+    it "should match amazon" do
+      expected = @exernal_services['other_webservice']['api_key']
+      Configer::EXTERNAL_SERVICES['other_webservice']['api_key'].should == expected
+    end
+  end
+
+  after do
+    Configer.destroy_constants
   end
   
 end
