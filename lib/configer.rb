91003d84ae90ca333ada9193f1b77f3350275495
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2009-11-24 23:10:13 -0500

    adding ability to set custom prefix for config constance

--- lib/configer.rb
+++ lib/configer.rb
@@ -8,11 +8,12 @@ require 'ruby-debug'
 module Configer
   VERSION = '0.0.1'
 
-  def self.init(path=File.dirname(__FILE__) + "/../spec/fixtures/")
+  def self.init(prefix=nil, path=File.dirname(__FILE__) + "/../spec/fixtures/")
     Dir["#{path}/*.yml","#{path}/*.yaml"].each do |path_to_config_file|
       config = File.basename(path_to_config_file).gsub(/\.y(a)?ml/, "")
       const_set(config.upcase, YAML.load_file(path_to_config_file))
     end
+    Object.class_eval "#{prefix} = #{Configer}"
   end
 
   def self.destroy_constants
--- spec/configer_spec.rb
+++ spec/configer_spec.rb
@@ -2,9 +2,13 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe "after initialization" do
   before do
-    Configer.init
+    Configer.init('MyApp')
     @hosts = YAML.load_file(File.dirname(__FILE__) + "/../spec/fixtures/hosts.yaml")
     @exernal_services = YAML.load_file(File.dirname(__FILE__) + "/../spec/fixtures/external_services.yaml")
+  end
+
+  it "should be able to use custom prefix" do
+    lambda { MyApp }.should_not raise_error NameError
   end
   
   it "should have HOSTS constance" do
@@ -19,12 +23,14 @@ describe "after initialization" do
     it "should match database" do
       @hosts['database'].each_pair do |key, value|
       Configer::HOSTS['database'][key].should == value
+      MyApp::HOSTS['database'][key].should == value
       end
     end
 
     it "should match search_node" do
       @hosts['search_node'].each_pair do |key, value|
       Configer::HOSTS['search_node'][key].should == value
+      MyApp::HOSTS['search_node'][key].should == value
       end
     end
   end
@@ -33,11 +39,13 @@ describe "after initialization" do
     it "should match amazon" do
       expected = @exernal_services['other_webservice']['api_key']
       Configer::EXTERNAL_SERVICES['other_webservice']['api_key'].should == expected
+      MyApp::EXTERNAL_SERVICES['other_webservice']['api_key'].should == expected
     end
   end
 
   after do
     Configer.destroy_constants
+    Object.send(:remove_const, :MyApp)
   end
   
 end
